057a7630c8c267d48c110420f0400962eafc9af3
Mason <bevinsm355@gmail.com>
2025-10-30 19:33:42 -0400

    Adjusted Home page and changed some styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,8 @@ import '../styles/Home.css';
 export default function Home() {
   return (
     <div className='home-welcome'>
-      <h1>Welcome to Dinotopia</h1>
-      <p>Explore the world of dinosaurs!</p>
+      <h1>Back then...</h1>
+      <p>Million of years ago, the earth we reside on, was once very different. Ruled by the most powerful animals the world has ever seen in histroy. Across multiple time periods have these creatrues walked and now you have a chance to learn about these animals in their glory. Venture on and be amazed of the great rulers of earth from the past.</p>
     </div>
   )
 }
