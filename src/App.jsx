c24606c632f021e435dea7aad8dab050f3c775a0
Mason <bevinsm355@gmail.com>
2025-11-01 23:06:59 -0400

    Changed color scheme and added some facts to home page.

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,22 @@ import Navbar from "./components/NavBar";
 import Home from "./pages/Home";
 import Catalog from "./pages/Catalog";
 import DinoCard from "./pages/DinoCard";
+import './styles/App.css';
 
 function App() {
   return (
     <div>
-      <img src="../src/assets/textures/BG.jpg" alt="Background" className="fixed top-0 left-0 w-full h-full object-cover -z-10 opacity-30"/>
-      <Navbar/>
+      <div className="background"></div>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path='/catalog' element={<Catalog/>}/>
-        <Route path='/dino/:id' element={<DinoCard/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/dino/:id" element={<DinoCard />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+
+export default App;
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,9 @@ export default function Home() {
     <div className='home-welcome'>
       <h1>Back then...</h1>
       <p>Million of years ago, the earth we reside on, was once very different. Ruled by the most powerful animals the world has ever seen in histroy. Across multiple time periods have these creatrues walked and now you have a chance to learn about these animals in their glory. Venture on and be amazed of the great rulers of earth from the past.</p>
+      <button className="jounrey-btn"> Begin Jounrey </button>
+      <section>Millions of years ago, the earth was a very different place. The continents that we know now used to all be connected into one massive continent named Pangea. This once massive supercontinent began breaking apart around 200 million years ago due to the techtonic plates within the Earth moving around. It kept moving until all the of Pangea was broken into smaller continents which then formed into the ones we know today.</section>
+      <section>This is where the 3d earth model will go.</section>
     </div>
   )
 }
