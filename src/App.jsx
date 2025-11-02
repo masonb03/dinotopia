42a0ebf6e20cd7d406d04c81159c13463070fa32
Mason <bevinsm355@gmail.com>
2025-11-01 22:23:40 -0400

    Added background and a about component/css

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import Home from "./pages/Home";
 import Catalog from "./pages/Catalog";
-import SingleDino from "./pages/SingleDino";
+import DinoCard from "./pages/DinoCard";
 
 function App() {
   return (
     <div>
+      <img src="../src/assets/textures/BG.jpg" alt="Background" className="fixed top-0 left-0 w-full h-full object-cover -z-10 opacity-30"/>
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path='/catalog' element={<Catalog/>}/>
-        <Route path='/dino/:id' element={<SingleDino/>}/>
+        <Route path='/dino/:id' element={<DinoCard/>}/>
       </Routes>
     </div>
   )
